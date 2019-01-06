7ca58bb2d2ffeb0273807b7647dd6e95ed8ab3f8
iOS Training <iostraining@ganeshs-macbook-pro.local>
2019-01-06 16:19:59 +0530

    Initial Commit

---
+++ pickerViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pickerViewPractice
+//
+//  Created by iOS Training on 1/6/19.
+//  Copyright © 2019 iOS Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
